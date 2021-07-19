5037d5f84076b14ad12718ca9b2b931bb8a34dec
hyechan <dhgpcks123@naver.com>
2021-07-19 14:11:39 +0900

    문자, 파일 parts로 넘어오는 거 확인

---
+++ src/main/java/com/springboot/upload_inf/controller/ServletUploadControllerV1.java
@@ -0,0 +1,38 @@
+package com.springboot.upload_inf.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+    @GetMapping("/upload")
+    public String newFile(){
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException{
+        log.info("request={}", request);
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts();
+        //이 parts가 부분별로 나눠진 거 각각 받아볼 수 있는 것임!
+        log.info("parts={}", parts);
+
+        return "upload-form";
+
+    }
+}
